c232a745fdebb7f2bcef965becdde239f249f3a8
liufangyang <liufangyang@ufl.edu>
2016-07-19 03:10:20 -0400

    learn how to push a project onto github

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherForcastApp.Models;
+
+namespace WeatherForcastApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetWeatherForcastByCityAndState(string city , string state)
+        {
+            Weather wea = new Weather();
+            return Json(wea.GetWeatherForcastByCityAndState(city , state), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetWeatherForcastDefault()
+        {
+            Weather wea = new Weather();
+            return Json(wea.GetWeatherForcastDefault(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/Weather.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherForcastApp.Models
+{
+    public class Weather
+    {
+        public Object GetWeatherForcastByCityAndState(string city , string state)
+        {
+            if(city == null || state == null)
+            {
+                return null;
+            }
+            //return jsonContent
+            string apiKey = "1d53ea7c0b4a3e80";
+            string url = "http://api.wunderground.com/api/" + apiKey + "/geolookup/conditions/q/" + state + "/" + city + ".json";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+        public Object GetWeatherForcastDefault()
+        {
+            
+            //return jsonContent
+            string apiKey = "1d53ea7c0b4a3e80";
+            string state = "Fl";
+            string city = "Orlando";
+            string url = "http://api.wunderground.com/api/" + apiKey + "/geolookup/conditions/q/" + state + "/" + city + ".json";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+
+    }
+}
